4b0d20700efd0d7fe9fba460d10d85c229a55a69
George Theofilis <theofilis.g@gmail.com>
2020-03-03 09:23:44 +0000

    Bump version: 0.3.0 → 0.11.0

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/chariot-h2020/chariot-firmware-upload-service',
-    version='0.3.0',
+    version='0.11.0',
     zip_safe=False,
 )
